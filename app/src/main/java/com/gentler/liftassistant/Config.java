b9d0f5492413e1a70bec014d9c52b2ca1d65066d
jiantao <eccfan@163.com>
2017-07-21 13:44:30 +0800

    initial commit

---
+++ app/src/main/java/com/gentler/liftassistant/Config.java
@@ -0,0 +1,13 @@
+package com.gentler.liftassistant;
+
+/**
+ * Created by administrato on 2017/7/12.
+ */
+
+public class Config {
+
+    private static final String PKG="com.gentler.liftassistant";
+    
+
+
+}
